4fe2106acfe80bb239c5c325f65d400e207f79e9
Asmit1709 <119005553+Asmit1709@users.noreply.github.com>
2023-11-06 00:09:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import { Form } from "./modules/form";
 import "./App.css";
-import { Dashboard } from "./modules/dashboard";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
 } from "react-router-dom";
-
-const Protectedroutes = ({ children }) => {
-  const isloggedin = localStorage.getItem("user: token") !== null || true;
-
-  if (!isloggedin) {
-    return <Navigate to={"/user/signup"} />;
-  }
-
-  return children;
-};
 
 function App() {
   return (
@@ -26,14 +15,7 @@ function App() {
           <Route path="/user/signup" element={<Form signedin={false} />} />
           <Route path="/user/register" element={<Form signedin={false} />} />
           <Route path="/user/signin" element={<Form signedin={true} />} />
-          <Route
-            path="/user/dashboard"
-            element={
-              <Protectedroutes>
-                <Dashboard />
-              </Protectedroutes>
-            }
-          />
+        
         </Routes>
       </Router>
     </div>
